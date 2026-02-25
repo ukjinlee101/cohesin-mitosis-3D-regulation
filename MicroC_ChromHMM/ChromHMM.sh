a0dcd06f79b3e8569c3b452586418c791b967050
UkJin Lee <ukjinlee@outlook.com>
2026-02-24 23:17:02 -0500

    edit

--- MicroC_ChromHMM/ChromHMM.sh
+++
@@ -1,3 +0,0 @@
-java -mx12000M -jar /Users/ukjinlee/Software/ChromHMM/ChromHMM.jar OverlapEnrichment -labels /Volumes/UKJIN_SSD/MtoG1_analysis_code/reference/chromHMM_fullstack/mm10_100_segments_segments.bed /Volumes/UKJIN_SSD/data/loop_chromosight/chromo_cons_allRes_postprocessed_anchor.bed mm10_100_segments
-
-java -mx12000M -jar /Users/ukjinlee/Software/ChromHMM/ChromHMM.jar OverlapEnrichment -uniformscale -labels /Volumes/UKJIN_SSD/MtoG1_analysis_code/reference/chromHMM_fullstack/mm10_100_segments_segments_grouped.bed anchors.bed mm10_100_segments_grouped
